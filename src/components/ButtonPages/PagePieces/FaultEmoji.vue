a2e911b1d4bba1f5086e470f1784293140869f8b
Brian Wolf <wolfbj32@gmail.com>
2025-03-13 21:04:04 -0400

    added emoji to indicate second serve

---
+++ src/components/ButtonPages/PagePieces/FaultEmoji.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="!scoreStore.secondServe">
+    <v-icon>mdi-tennis-ball</v-icon>
+  </div>
+  <!-- <div v-else>
+    <v-icon>mdi-tennis-ball</v-icon><v-icon>mdi-tennis-ball</v-icon>
+  </div> -->
+</template>
+
+<script>
+import { useButtonStore } from "@/stores/buttonStores"; // Import your Pinia store
+import { useMatchScoreStore } from "@/stores/matchScoreStore";
+
+export default {
+  setup() {
+    const scoreStore = useMatchScoreStore();
+    return { scoreStore };
+  },
+};
+</script>
