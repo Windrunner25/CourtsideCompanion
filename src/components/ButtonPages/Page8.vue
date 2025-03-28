0407e0315f607fb9b01f89e65bc50008ab0abf9b
Brian Wolf <wolfbj32@gmail.com>
2025-03-28 19:14:49 -0400

    page 8

---
+++ src/components/ButtonPages/Page8.vue
@@ -0,0 +1,17 @@
+<template>
+  <ShotIntent />
+  <ShotSide />
+  <ShotType />
+  <ShotDirection />
+  <ErrorLocation />
+  <RallyLength />
+</template>
+
+<script setup lang="ts">
+import RallyLength from "./ShotCharacteristics/RallyLength.vue";
+import ErrorLocation from "./ShotCharacteristics/ErrorLocation.vue";
+import ShotDirection from "./ShotCharacteristics/ShotDirection.vue";
+import ShotIntent from "./ShotCharacteristics/ShotIntent.vue";
+import ShotSide from "./ShotCharacteristics/ShotSide.vue";
+import ShotType from "./ShotCharacteristics/ShotType.vue";
+</script>
