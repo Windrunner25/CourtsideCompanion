51d78df01d0cfcc94da9a6a4d263ab888c15f599
Brian Wolf <wolfbj32@gmail.com>
2025-03-29 11:25:07 -0400

    added conditional checks to prevent null from being added

--- src/components/Interface/SubmitButton.vue
+++ src/components/Interface/SubmitButton.vue
@@ -29,18 +29,30 @@ export default {
   },
   methods: {
     submitPoint() {
-      this.scoreStore.currentPoint["Stroke Intent"] =
-        this.buttonStore.group1Active;
-      this.scoreStore.currentPoint["Stroke Side"] =
-        this.buttonStore.group2Active;
-      this.scoreStore.currentPoint["Stroke Type"] =
-        this.buttonStore.group3Active;
-      this.scoreStore.currentPoint["Stroke Direction"] =
-        this.buttonStore.group4Active;
-      this.scoreStore.currentPoint["Error Location"] =
-        this.buttonStore.group5Active;
-      this.scoreStore.currentPoint["Rally Length"] =
-        this.buttonStore.group6Active;
+      if(this.buttonStore.group1Active !== null) {
+        this.scoreStore.currentPoint["Stroke Intent"] =
+          this.buttonStore.group1Active;
+      }
+      if(this.buttonStore.group2Active !== null) {
+        this.scoreStore.currentPoint["Stroke Side"] =
+          this.buttonStore.group2Active;
+      }
+      if(this.buttonStore.group3Active !== null) {
+        this.scoreStore.currentPoint["Stroke Type"] =
+          this.buttonStore.group3Active;
+      }
+      if(this.buttonStore.group4Active !== null) {
+        this.scoreStore.currentPoint["Stroke Direction"] =
+          this.buttonStore.group4Active;
+      }
+      if(this.buttonStore.group5Active !== null) {
+        this.scoreStore.currentPoint["Error Location"] =
+          this.buttonStore.group5Active;
+      }
+      if(this.buttonStore.group6Active !== null) {
+        this.scoreStore.currentPoint["Rally Length"] =
+          this.buttonStore.group6Active;
+      }
 
       this.scoreStore.pointEnded(
         this.matchInfoStore.getPlayerNameFromSide(this.buttonStore.side)
