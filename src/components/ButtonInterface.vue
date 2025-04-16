5ddf484599cf4850d0c15274f022098a54056a55
Brian Wolf <wolfbj32@gmail.com>
2025-04-15 21:28:34 -0400

    new match summary page

--- src/components/ButtonInterface.vue
+++ src/components/ButtonInterface.vue
@@ -58,7 +58,8 @@
         <Page7 />
       </div>
       <div v-show="buttonStore.page === 8">
-        <MatchSummary />
+        <!-- <MatchSummary /> -->
+         <AnalyticsPlaceholder/>
         <TwoPlayerChart/>
       </div>
 
@@ -95,8 +96,9 @@ import SubmitButton from "./Interface/SubmitButton.vue";
 import NewMatchForm from "./Interface/NewMatchForm.vue";
 import NewMatchView from "./Interface/NewMatchView.vue";
 import ScoreCard from "./Interface/ScoreCard.vue";
-import MatchSummary from "./Interface/MatchSummary.vue";
+import MatchSummary from "./Analytics/MatchSummary.vue";
 import TwoPlayerChart from "./Analytics/TwoPlayerChart.vue"
+import AnalyticsPlaceholder from "./Analytics/AnalyticsPlaceholder.vue";
 
 export default {
   components: {
